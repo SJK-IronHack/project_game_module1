bae2547374dfce7b1faef6db67a59d2d50d11a35
mirJakub <slawomir.krzyzak@gmail.com>
2023-12-04 16:35:51 +0100

    doesnt work at all

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ restartButton.addEventListener('click', function () {
   // startGame()
   location.reload()
 })
+document.addEventListener('keydown', event => {
+
+  if (event.code === 'ArrowLeft') {
+    console.log('Go left !')
+    game.player.directionX = -1
+  }
+  if (event.code === 'ArrowRight'){
+    console.log('Go right !')
+    game.player.directionX = 1
+  }
+})
+
+document.addEventListener('keyup', event => {
+  if (
+    event.code === 'ArrowLeft' ||
+    event.code === 'ArrowRight' 
+  ) {
+    console.log('Stop moving on X')
+    game.player.directionX = 0
+  }
+})
